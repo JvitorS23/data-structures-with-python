7b58e50932c95b16fbbf63c3d1ef2ca7f555561a
José <jose.santos@conceptu.ind.br>
2022-02-28 08:54:47 -0300

    Graphs

---
+++ graph/graph.py
@@ -0,0 +1,37 @@
+class Graph:
+
+    def __init__(self):
+        self.adj_list = {}
+
+    def print_graph(self):
+        for key in self.adj_list.keys():
+            print(key, ': ', self.adj_list[key])
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list:
+            self.adj_list[vertex] = []
+
+    def add_edge(self, v1, v2):
+        if v1 not in self.adj_list.keys() or v2 not in self.adj_list.keys():
+            return False
+        self.adj_list[v1].append(v2)
+        self.adj_list[v2].append(v1)
+        return True
+
+    def remove_edge(self, v1, v2):
+        if v1 not in self.adj_list.keys() or v2 not in self.adj_list.keys():
+            return False
+        try:
+            self.adj_list[v1].remove(v2)
+            self.adj_list[v2].remove(v1)
+        except ValueError:
+            pass
+        return True
+
+    def remove_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            return False
+        for other_vertex in self.adj_list[vertex]:
+            self.adj_list[other_vertex].remove(vertex)
+        del self.adj_list[vertex]
+        return True
